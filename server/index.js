76ddd5d68b54455d448bd032482d8bdaccdb9e12
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-12-09 09:49:38 +0600

    Messed up with websocket

--- server/index.js
+++ server/index.js
@@ -1,18 +1,24 @@
-import express from "express";
+// import express from "express";
 import { WebSocketServer } from "ws";
 
-const app = express();
-const port = 3000;
+// const app = express();
+// const port = 3000;
 
-const server = app.listen(port, () => {
-  console.log("Server is listening...");
-});
+// const server = app.listen(port, () => {
+//   console.log("Server is listening...");
+// });
 
-const wss = new WebSocketServer({ server });
+const server = new WebSocketServer.Server({ port: "8080" });
 
-wss.on("connection", (ws) => {
-  ws.on("message", (data) => {
-    console.log("Data from client : ", data);
-    ws.send("Jajak Allah Khayran");
+server.on("connection", (socket) => {
+  socket.on("message", (message) => {
+    socket.send(`Roger that! ${message}`);
   });
 });
+
+// wss.on("connection", (ws) => {
+//   ws.on("message", (data) => {
+//     console.log("Data from client : ", data);
+//     ws.send("Jajak Allah Khayran");
+//   });
+// });
